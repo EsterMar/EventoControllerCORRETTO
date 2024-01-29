3ddfa876c1a5b58498770923d93b3c9a5fff7543
EsterMar <96837672+EsterMar@users.noreply.github.com>
2024-01-29 21:23:30 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+
+import  com.example.demo.authentication.Utils;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+
+    @GetMapping("/welcome")
+    @PreAuthorize("hasAuthority('view-profile')")
+    public String home(@RequestParam(value = "someValue") int value) {
+        return "Welcome, " + Utils.getEmail() + " " + value + " !";
+    }
+
+
+}
+
+
